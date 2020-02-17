e5f89d4a6021fcdecabf1a679e110bf454f41472
Edgar <edgartorresfreitas@gmail.com>
2020-02-17 09:08:07 -0300

    CQRS api read-only

---
+++ DDDSample.WebAPI.Helpers/RetornoHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace DDDSample.WebAPI.Helpers
+{
+    public class RetornoHelper
+    {
+        public static IActionResult CreateResponse(object obj, int statusCode)
+        {
+            return new JsonResult(obj)
+            {
+                StatusCode = statusCode
+            };
+        }
+    }
+}
---
+++ DDDSample.WebApi.ReadOnly/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DDDSample.WebApi.ReadOnly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+
+        public static IActionResult CreateResponse(object obj, int statusCode)
+        {
+            return new JsonResult(obj)
+            {
+                StatusCode = statusCode
+            };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+
+            var tsc = new TaskCompletionSource<IActionResult>();
+            try
+            {
+                //1 - Criar uma conexão direta com o banco
+                //2 - exeutar o select que quiser
+                //3 - Retornar o resultado do select
+                
+                tsc.SetResult(CreateResponse("ok", 200));
+            }
+            catch (Exception e)
+            {
+                tsc.SetResult(CreateResponse(e.Message, 500));
+            }
+            return await tsc.Task;
+        }
+
+    }
+}
